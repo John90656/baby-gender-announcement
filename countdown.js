d8644da918fc4c23ffbd84b4614919bdbdbe0151
John90656 <46474061+John90656@users.noreply.github.com>
2023-11-23 08:42:20 +0100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,67 +1,52 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Set the date and time for the gender reveal
-    const revealDate = new Date("2023-11-23T10:00:00");
-    const gender = "boy"; // Change to "girl" for a girl reveal
+const revealDate = new Date("2023-11-23T10:00:00");
+const boyText = document.getElementById("boyText");
+const girlText = document.getElementById("girlText");
+const genderContent = document.getElementById("genderContent");
 
-    function updateCountdown() {
-        const currentDate = new Date();
-        const timeDifference = revealDate - currentDate;
+function updateCountdown() {
+    const currentDate = new Date();
+    const difference = revealDate - currentDate;
 
-        if (timeDifference > 0) {
-            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    if (difference <= 0) {
+        // Gender reveal time
+        document.getElementById("countdownDisplay").innerHTML = "It's time to reveal!";
+        boyText.style.visibility = "hidden";
+        girlText.style.visibility = "hidden";
+        genderContent.style.display = "block";
 
-            const countdownDisplay = document.getElementById("countdownDisplay");
-            countdownDisplay.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        // Change background color to pink
+        document.body.style.backgroundColor = "#FFC0CB"; // pastel pink
+    } else {
+        // Countdown
+        const seconds = Math.floor(difference / 1000);
 
-            // Intermittent teasing background color change
-            const teaseInterval = 10000; // Change the interval in milliseconds (e.g., 10000 for 10 seconds)
-            if (timeDifference % teaseInterval < 1000) {
-                teaseBackground();
-            }
+        // Toggle every 5 seconds
+        const isBlue = seconds % 10 < 5; // Switch every 10 seconds
 
-            toggleTextVisibility(); // Toggle visibility of text elements
-            setTimeout(updateCountdown, 1000);
-        } else {
-            // Timer has reached zero, reveal the gender
-            const genderContent = document.getElementById("genderContent");
-            genderContent.style.display = "block";
-            const countdownDisplay = document.getElementById("countdownDisplay");
-            countdownDisplay.innerHTML = "Gender Revealed!";
-
-            // Change background color based on gender
-            document.body.style.transition = "background-color 2s ease";
-            document.body.style.backgroundColor = gender === "boy" ? "#add8e6" : "#ffc0cb";
-        }
-    }
-
-    function teaseBackground() {
-        // Tease the user with a background color change between pink and blue
-        document.body.style.transition = "background-color 5s ease";
-
-        // Toggle between pink and blue based on the current background color
-        if (document.body.style.backgroundColor === "rgb(173, 216, 230)") {
-            document.body.style.backgroundColor = "#ffc0cb"; // Pink
-        } else {
-            document.body.style.backgroundColor = "#add8e6"; // Blue
-        }
-    }
-
-    function toggleTextVisibility() {
-        const boyText = document.getElementById("boyText");
-        const girlText = document.getElementById("girlText");
-
-        if (document.body.style.backgroundColor === "rgb(173, 216, 230)") {
+        if (isBlue) {
+            // Blue state
+            document.body.style.backgroundColor = "#add8e6"; // light blue
             boyText.style.visibility = "visible";
             girlText.style.visibility = "hidden";
         } else {
+            // Pink state
+            document.body.style.backgroundColor = "#FFC0CB"; // pastel pink
             boyText.style.visibility = "hidden";
             girlText.style.visibility = "visible";
         }
-    }
 
-    // Initial call to start the countdown
-    updateCountdown();
-});
+        // Display the countdown timer
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const remainingSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        document.getElementById("countdownDisplay").innerHTML = `${days}d ${hours}h ${minutes}m ${remainingSeconds}s`;
+    }
+}
+
+// Initial call to set up the initial state
+updateCountdown();
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
